310bc2fe03688311fc951ec633c678acdec8ecf2
infustech <dev@infus.com.tr>
2025-05-14 17:34:05 +0300

    Demand Type

---
+++ nuted_hr/nuted_hr/doctype/demand_type/demand_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, infus and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Demand Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ nuted_hr/nuted_hr/doctype/demand_type/demand_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, infus and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class DemandType(Document):
+	pass
